89515e07327a3dad835ad92ea385eacd434d30e5
Mitchell-Crumbley <mitchellcrumbley@gmail.com>
2021-03-13 03:06:24 -0600

    added the logged out view

--- src/javascripts/components/loginButton.js
+++ src/javascripts/components/loginButton.js
@@ -2,10 +2,12 @@ import signIn from '../helpers/auth/signIn';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = `<div>
+  const domString = `<nav class="navbar navbar-expand-lg navbar-dark mb-4" style="background-color: rgb(26, 24, 24); justify-content-space-evenly;">
+  <img src="https://i1.wp.com/www.authormedia.com/wp-content/uploads/2013/04/pinterest_badge_red.png?resize=800%2C675&ssl=1" style="width: 75px; height: 65px;>
   <div class="image" href="#"></div>
-  <button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>
-</div>`;
+  <button style="margin-left: auto;" id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>
+</nav>
+<h2>Please Log In!</h2>`;
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
--- src/javascripts/helpers/data/router.js
+++ src/javascripts/helpers/data/router.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../../components/loginButton';
-import logoutButton from '../../components/logoutButton';
 import startApp from '../../views/startApp';
 import firebaseConfig from '../auth/apiKeys';
 
@@ -12,7 +11,6 @@ const checkLoginStatus = () => {
       startApp(user);
     } else {
       // person is NOT logged in
-      logoutButton();
       loginButton();
     }
   });
